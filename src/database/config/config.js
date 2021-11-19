ef5206758c23853bfbf622e3ffbfcce1eb71dea0
santheartist <vazquezbare@gmail.com>
2021-11-19 14:22:13 -0300

    config.js settings

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,16 +7,16 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "oq574s0afyequbau",
+    "password": "jl99pxj70q3twblv",
+    "database": "dlgm02gx2j3xt5hg",
+    "host": "w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "rooq574s0afyequbauot",
+    "username": "oq574s0afyequbau",
     "password": "jl99pxj70q3twblv",
-    "database": "database_production",
+    "database": "dlgm02gx2j3xt5hg",
     "host": "w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
